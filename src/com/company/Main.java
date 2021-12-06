3f1e1b1599b665850e55afb58a5c1e2a5038a61f
s038738 <s038738@ad.viko.lt>
2021-12-06 11:18:53 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import javax.swing.*;
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("CUM");
+    }
+}
